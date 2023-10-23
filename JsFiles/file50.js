ecea9a3984d5ed6f84d959a0176536a3412420b8
Aditya Patil <aadityapatil8012@gmail.com>
2023-10-23 17:14:06 +0530

    done with the functions, learned about rest, destructuring parameters, callback and functions returning functions

--- JsFiles/file50.js
+++ JsFiles/file50.js
@@ -1,13 +1,28 @@
 // call back function;
 
-function myFunc(a2){
-  console.log("hello there I am a func and I can...");
-  a2("Aditya");
+// function myFunc(a2){
+//   console.log("hello there I am a func and I can...");
+//   a2("Aditya");
+// }
+
+// function myFunc2(name){
+//   console.log("inside myFunc2");
+//   console.log(`My name is ${name}`)
+// }
+
+// myFunc(myFunc2);  
+
+// function returning function;
+
+function myFunc(){
+    function hello(){
+      return "Hello World";
+    }
+    return hello;
 }
 
-function myFunc2(name){
-  console.log("inside myFunc2");
-  console.log(`My name is ${name}`)
-}
+const ans = myFunc();
+console.log(ans());
 
-myFunc(myFunc2);
+
+
