5984957227790d5591488a47f261dc6d265b7230
Aditya Patil <aadityapatil8012@gmail.com>
2023-11-24 19:34:00 +0530

    tooks some break but this is the a new start

---
+++ whoJs/wFile8.js
@@ -0,0 +1,8 @@
+// get and set attributes;
+
+const link = document.querySelector("a");
+console.log(link.getAttribute("href"));
+
+link.setAttribute("href", "#AdityaHome");
+
+console.log(link.getAttribute("href"));
---
+++ whoJs/wFile9.js
@@ -0,0 +1,7 @@
+// get multiple elements using getElements by class name;
+// get multiple elements using querySeledtorAll;
+
+// const navItems = document.getElementsByClassName("nav-item");
+const navItems = document.querySelectorAll(".nav-item");
+console.log(navItems);
+
